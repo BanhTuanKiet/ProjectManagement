682cba33671d4ad860591eaba0bb23280df6b006
bunmasv2 <thaibao5335@gmail.com>
2025-10-13 13:25:52 +0700

    ...

--- client/src/app/(private)/project/layout.tsx
+++ client/src/app/(private)/project/layout.tsx
@@ -8,11 +8,11 @@ export default function Layout({ children }: { children: React.ReactNode }) {
 
     return (
         <SidebarProvider>
-            <div className="flex flex-col w-full h-full overflow-hidden"> {/* ✅ dùng h-full + overflow-hidden */}
+            <div className="flex flex-col w-full h-screen"> {/* ✅ dùng h-full + overflow-hidden */}
                 <ProjectHeader sidebarTrigger={<SidebarTrigger />} />
                 <div className="flex flex-1 overflow-hidden">
                     <SidebarCustom className="mt-13" />
-                    <main className="flex-1 overflow-y-auto bg-gray-50">
+                    <main className="flex-1 overflow-auto bg-gray-50">
                         {children}
                     </main>
                 </div>
